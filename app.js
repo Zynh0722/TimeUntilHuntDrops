8fd5897139740088c00adebeb922de458eecfb44
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-23 03:35:32 -0800

    fix date

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-function timeToGo(s) {
-  var now = new Date;
-  var utc_timestamp = new Date(Date.UTC(now.getFullYear(),now.getMonth(), now.getDate() ,
-      now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()));
-  var diff = new Date(s.getTime() - utc_timestamp.getTime());
-
-
-  out = '';
-  out += diff.getHours() + " hours, ";
-  out += diff.getMinutes() + " minutes, "
-  out += diff.getSeconds() + " seconds"
-
-
-  return out;
-}
-
 $(document).ready(function(){
   var $app = $('#app');
 
@@ -21,7 +5,7 @@ $(document).ready(function(){
   var $topBox = $('<div class="topBox"></div>');
   var $bottomBox = $('<div class="bottomBox"></div>')
 
-  var dropsTime = new Date('2022-02-23T16:00:00+0000');
+  var dropsTime = new Date('2022-02-23T15:00:00+0000');
 
   $topBox.appendTo($centerbox);
   $bottomBox.appendTo($centerbox);
